60b308f87a685bfab8a2ed365fcec4996e1f60a5
Arav Chadha <97276472+AravCGitHub@users.noreply.github.com>
2024-07-03 14:17:27 -0400

    working

--- Code/advertiser.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class Advertiser:
-    def __init__(self, numTypes):
-        self.budget = random.randint(1, 100)
-        self.valuation = []
-        for i in range(numTypes):
-            self.valuation.append(random.expovariate(1))
--- synthetic instances/main.py
+++ synthetic instances/main.py
@@ -1,18 +1,32 @@
+import math
 import random
 from advertiser import Advertiser
 from impression import Impression
 
+def clamp(n, min, max): 
+    if n < min: 
+        return min
+    elif n > max: 
+        return max
+    else: 
+        return n 
+
 def syntheticInstance(numAds, numImps):
-    numTypes = 10
+    numTypes = 8
     ads = []
     for i in range(numAds):
         ads.append(Advertiser(numTypes))
     imps = []
     for i in range(numImps):
-        imps.append(Impression(random.randint(0, numTypes-1)))
+        sections = (6.0 / numTypes)
+        rand = random.gauss(0,1) + 3
+        type = clamp(math.floor(0.5 + (rand / sections)), 1, numTypes)
+        imps.append(Impression(type))
     return ads, imps
 
-a, i = syntheticInstance(3,5)
+a, i = syntheticInstance(5,3)
 
-for x in a:
-    print(x)
+for x1 in i:
+    print(x1)
+for x2 in a:
+    print(x2)
