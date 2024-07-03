945866a5a95fe1eaadddbc850805b0cfc8bb7a02
Arav Chadha <97276472+AravCGitHub@users.noreply.github.com>
2024-07-03 11:23:37 -0400

    initial commit

---
+++ Code/main.py
@@ -0,0 +1,16 @@
+import random
+# import advertiser
+import impression
+
+# def syntheticInstance(numAds, numImps):
+#     numTypes = 10
+#     ads = []
+#     for i in range(numAds):
+#         ads.append(advertiser.Advertiser(numTypes))
+#     imps = []
+#     for i in range(numImps):
+#         imps.append(impression.Impression(random.randint(0, numTypes-1)))
+#     return ads, imps
+
+# print(syntheticInstance(3,5))
+impression.test()
