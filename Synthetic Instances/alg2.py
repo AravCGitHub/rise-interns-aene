0daf5da4f8334dc3e4fb994b67c7cee0d36b6e5d
Arav Chadha <97276472+AravCGitHub@users.noreply.github.com>
2024-07-15 18:35:19 -0400

    working alg 2

---
+++ Synthetic Instances/alg2.py
@@ -0,0 +1,59 @@
+import time
+import numpy as np
+import math
+
+def calculateD(adv, imp, lam):
+    val = adv.valuations[imp.type]
+    D = math.e ** (val / lam - 1)
+    return D
+
+def minInd(arr):
+    min = np.argmax(np.array(arr))
+    for i in range(len(arr)):
+        if (arr[i] < arr[min] and arr[i] != 0):
+            min = i
+    return min
+
+def solve(advs, imps, weights, lam, eps, numRounds):
+    xMatrix = np.zeros((len(advs), len(imps)))
+    priorityScores = [(1+eps) ** -numRounds] * len(advs)
+    startTime = time.time()
+    for r in range(numRounds):
+        # Step 1
+        for i in range (len(imps)):
+            sum = 0
+            for a in range (len(advs)):
+                D_a = calculateD(advs[a], imps[i], lam)
+                sum += priorityScores[a] * D_a
+            for a in range (len(advs)):
+                if (sum <= 1):
+                    xMatrix[a][i] = priorityScores[a] * calculateD(advs[a], imps[i], lam)
+                else:
+                    xMatrix[a][i] = (priorityScores[a] * calculateD(advs[a], imps[i], lam)) / sum
+        # Step 2
+        for a in range (len(advs)):
+            alloc = xMatrix[a].sum()
+            if (alloc <= (advs[a].budget) / (1 + eps)):
+                priorityScores[a] = (1 + eps) * priorityScores[a]
+            if (alloc >= (advs[a].budget) * (1 + eps)):
+                priorityScores[a] = priorityScores[a] / (1 + eps)
+    # Final Check
+    for a in range (len(advs)):
+        if (xMatrix[a].sum() > advs[a].budget):
+            print("Check Adv:",a)
+            valList = []
+            for i in range (len(imps)):
+                valList.append(-weights[a*len(imps) + i] * xMatrix[a][i])
+            # print(valList)
+            count = 0
+            while (xMatrix[a].sum() > advs[a].budget):
+                # print(valList)
+                minIndex = minInd(valList)
+                dif = xMatrix[a].sum() - advs[a].budget
+                if (xMatrix[a][minIndex] > dif):
+                    xMatrix[a][minIndex] -= dif
+                else:
+                    xMatrix[a][minIndex] = 0
+                valList[minIndex] = -weights[a*len(imps) + minIndex] * xMatrix[a][minIndex]
+    endTime = time.time()
+    return xMatrix.ravel(), endTime - startTime
--- synthetic instances/main.py
+++ synthetic instances/main.py
@@ -4,6 +4,7 @@ import math
 import time
 import optimal
 import alg1
+import alg2
 import matplotlib.pyplot as plt
 from advertiser import Advertiser
 from impression import Impression
@@ -24,64 +25,11 @@ def objectiveValue(solved, weights):
     dot = np.dot(npSolv * -1, weights)
     return dot
 
-def calculateD(adv, imp, lam):
-    val = adv.valuations[imp.type]
-    D = math.e ** (val / lam - 1)
-    return D
-
-def alg2(advs, imps, lam, eps, numRounds):
-    xMatrix = np.zeros((len(advs), len(imps)))
-    priorityScores = [(1+eps) ** -numRounds] * len(advs)
-    for r in range(numRounds):
-        # Step 1
-        for i in range (len(imps)):
-            sum = 0
-            for a in range (len(advs)):
-                D_a = calculateD(advs[a], imps[i], lam)
-                sum += priorityScores[a] * D_a
-            for a in range (len(advs)):
-                if (sum <= 1):
-                    xMatrix[a][i] = priorityScores[a] * calculateD(advs[a], imps[i], lam)
-                else:
-                    xMatrix[a][i] = (priorityScores[a] * calculateD(advs[a], imps[i], lam)) / sum
-        # Step 2
-        for a in range (len(advs)):
-            alloc = xMatrix[a].sum()
-            if (alloc <= (advs[a].budget) / (1 + eps)):
-                priorityScores[a] = (1 + eps) * priorityScores[a]
-            if (alloc >= (advs[a].budget) * (1 + eps)):
-                priorityScores[a] = priorityScores[a] / (1 + eps)
-    # Final Check
-    for a in range (len(advs)):
-        if (xMatrix[a].sum() > advs[a].budget):
-            N_a = 0
-            for val in xMatrix[a]:
-                if (val > 0):
-                    N_a += 1
-            reductionFactor = advs[a].budget / N_a
-            for i in range (len(imps)):
-                xMatrix[a][i] = xMatrix[a][i] * reductionFactor
-                if (xMatrix[a].sum() <= advs[a].budget):
-                    break
-    return xMatrix
-        
-def test():
-    advs, imps = createSyntheticInstance(3, 5)
-    lam = 1
-    eps = 0.1
-    numRounds = 10
-    alg2Solved = alg2(advs, imps, lam, eps, numRounds)
-    weights = optimal.createVectorC(advs, imps)
-    obj = objectiveValue(alg2Solved, weights)
-    print (alg2Solved)
-    print("Objective Value:", obj)
-
-
 def main():
     figure, axis = plt.subplots(1, 2)
-    for count in range(1):
+    for count in range(20):
         numImps = random.randint(1,100)
-        a, i = createSyntheticInstance(10,100) # fixed 20 advs, rand 1-100 imps
+        a, i = createSyntheticInstance(10,numImps) # fixed 20 advs, rand 1-100 imps
         weights = optimal.createVectorC(a,i)
         # Optimal Algorithm
         optSolved, optTimeTaken = (optimal.lpSolve(a,i))
@@ -96,34 +44,46 @@ def main():
         print("Time Taken:", algTimeTaken)
         # print(algSolved)
         # Algorithm 2
-        alg2Solved = alg2(a,i,0.1,0.01,20) # lam = 0.1, eps = 1, numRounds = 20
+        alg2Solved, alg2TimeTaken = alg2.solve(a,i,weights,0.03,0.01,20) # lam = 0.1, eps = 1, numRounds = 20
         alg2Obj = objectiveValue(alg2Solved, weights)
         print("Alg2 Objective Value:", alg2Obj)
+        print("Time Taken:", alg2TimeTaken)
         # print(alg2Solved)
-    #     # Graphing Obj vs Imps
-    #     if (count == 1):
-    #         axis[0].plot(numImps, optObj, 'bo', label = "CVXOPT")
-    #         axis[0].plot(numImps, algObj, 'ro', label = "Algorithm 1")
-    #     else:
-    #         axis[0].plot(numImps, optObj, 'bo')
-    #         axis[0].plot(numImps, algObj, 'ro')
-    #     axis[0].set_xlabel('Number of Impressions')
-    #     axis[0].set_ylabel('Objective Value')
-    #     # Graphing Time vs Imps
-    #     if (count == 1):
-    #         axis[1].plot(numImps, optTimeTaken, 'bo', label = "CVXOPT")
-    #         axis[1].plot(numImps, algTimeTaken, 'ro', label = "Algorithm 1")
-    #     else:
-    #         axis[1].plot(numImps, optTimeTaken, 'bo')
-    #         axis[1].plot(numImps, algTimeTaken, 'ro')
-    #     axis[1].set_xlabel('Number of Impressions')
-    #     axis[1].set_ylabel('Time Taken')
-    # axis[0].legend()
-    # axis[1].legend()
-    # plt.tight_layout()
-    # plt.savefig('/Users/aravchadha/Documents/GitHub/rise-interns-aene/Images/image.png')
+        # Graphing Obj vs Imps
+        if (count == 1):
+            axis[0].plot(numImps, optObj, 'bo', label = "CVXOPT")
+            axis[0].plot(numImps, algObj, 'ro', label = "Algorithm 1")
+            axis[0].plot(numImps, alg2Obj, 'go', label = "Algorithm 2")
+        else:
+            axis[0].plot(numImps, optObj, 'bo')
+            axis[0].plot(numImps, algObj, 'ro')
+            axis[0].plot(numImps, alg2Obj, 'go')
+        axis[0].set_xlabel('Number of Impressions')
+        axis[0].set_ylabel('Objective Value')
+        # Graphing Time vs Imps
+        if (count == 1):
+            axis[1].plot(numImps, optTimeTaken, 'bo', label = "CVXOPT")
+            axis[1].plot(numImps, algTimeTaken, 'ro', label = "Algorithm 1")
+            axis[1].plot(numImps, alg2TimeTaken, 'go', label = "Algorithm 2")
+        else:
+            axis[1].plot(numImps, optTimeTaken, 'bo')
+            axis[1].plot(numImps, algTimeTaken, 'ro')
+            axis[1].plot(numImps, alg2TimeTaken, 'go')
+        axis[1].set_xlabel('Number of Impressions')
+        axis[1].set_ylabel('Time Taken')
+    axis[0].legend()
+    axis[1].legend()
+    plt.tight_layout()
+    plt.savefig('/Users/aravchadha/Documents/GitHub/rise-interns-aene/Images/image.png')
 
 main()
 # test()
 
 # make graph for time v. profit for several algorithms by varying synthetic instance size using matplotlib
+
+# make line graphs
+# bigger instances for alg1 and 2
+# rounds vs maximum budget overflow
+# same graphs on bad instances
+# make obj vs every parameter for alg2
+# make time vs every parameter for alg2
